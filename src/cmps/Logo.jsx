dbc8599eecb7ec2c514a374c14f85820b24bb74f
alonWohl <alonw1010@gmail.com>
2024-12-16 17:48:09 +0200

    Add link to logo

--- src/cmps/Logo.jsx
+++ src/cmps/Logo.jsx
@@ -1,14 +1,16 @@
+import { Link } from 'react-router-dom'
+
 const logoText = 'Mr Toy'
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'pink', 'cyan']
 
 export function Logo() {
 	return (
-		<div className="logo">
+		<Link to={'/'} className="logo">
 			{logoText.split('').map((char, index) => (
 				<span key={index} style={{ color: colors[index % colors.length] }}>
 					{char}
 				</span>
 			))}
-		</div>
+		</Link>
 	)
 }
